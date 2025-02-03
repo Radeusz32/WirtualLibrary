eb1b43be698817411dbb98430ce2b8c7aae182ac
Radek Wozniak <radeusz32@gmail.com>
2025-02-03 14:28:32 +0100

    dodanie komponentu obsługującego wszystkie podrzędne komponenty

---
+++ resources/js/App.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <h1>Wirtualna Biblioteka 📚</h1>
+        <label>👤 Użytkownik:</label>
+        <input v-model="user_name" placeholder="Wpisz swoje imię" />
+
+        <div class="app-container">
+    
+            <div class="forms-container">
+                <BookForm @bookAdded="handleNewBook"  />
+                <ReviewForm 
+                    :bookId="selectedBookId"
+                    :user_name="user_name"
+                    :books="books" 
+                    @reviewAdded="addReviewToList" 
+                />
+            </div>
+
+            
+            <div class="content-container">
+                <BookList ref="bookList" :user_name="user_name" />
+                <ReviewList ref="reviewList" :user_name="user_name" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import BookList from './components/BookList.vue';
+import BookForm from './components/BookForm.vue';
+import ReviewForm from './components/ReviewForm.vue'
+import ReviewList from './components/ReviewList.vue'
+export default {
+    components: { BookList, BookForm, ReviewForm, ReviewList },
+    data() {
+        return {
+            books: [],
+            selectedBookId: null,
+            user_name: '' // ✅ Przechowuje wpisane imię użytkownika
+        };
+    },
+    created() {
+    this.fetchBooks();
+  },
+    methods: {
+        fetchBooks() {
+      axios.get('/books')
+        .then(response => {
+          this.books = response.data;
+        })
+        .catch(error => console.error("❌ Błąd pobierania książek:", error));
+    },
+        handleNewBook(book) {
+            console.log("📥 Otrzymano zdarzenie `bookAdded` w App.vue:", book);
+            if (this.$refs.bookList) {
+                console.log("📝 Dodawanie książki do listy w BookList.vue");
+                this.$refs.bookList.addNewBook(book);
+            } else { 
+                console.error("❌ `bookList` nie jest poprawnie zainicjalizowane!");
+            }
+        },
+        addReviewToList(newReview) {
+            console.log("🔄 Przekazanie nowej recenzji do ReviewList.vue:", newReview);
+            this.$refs.reviewList.addReviewToList(newReview); // 🔥 PRZEKAZUJEMY RECENZJĘ DO LISTY
+        }
+        
+    }
+};
+</script>
+
+<style scoped>
+.app-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+}
+
+/* ✅ Stylizacja sekcji */
+.forms-container, .content-container {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 10px;
+    border: 2px solid #ccc;
+    border-radius: 8px;
+    background-color: #f9f9f9;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
+    gap: 20px;
+    flex:1;
+}
+.content-container{
+    display: flex;
+}
+</style>
